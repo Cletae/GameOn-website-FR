b23a6e84c2a11e0b5923a527d7819e68ce798753
Souvi <souvima@hotmail.com>
2021-08-16 18:11:15 +0200

    Add error function & checkLocation

--- modal.js
+++ modal.js
@@ -18,6 +18,9 @@ const lastName = document.getElementById("#last");
 const email = document.getElementById("#email");
 const birth = document.getElementById("#birthdate");
 const participation = document.getElementById("#quantity");
+const locations = document.getElementsByName("location");
+const checkbox1 = document.getElementsByClassName(".checkbox-icon");
+const checkbox2 = document.getElementById("#checkbox2");
 const close = document.querySelector(".close");
 const btnSubmit = document.getElementsByClassName(".btn-submit");
 
@@ -27,6 +30,7 @@ const emptyLastName = document.getElementById("#lastname-empty");
 const emptyEmail = document.getElementById("#mail-empty");
 const emptyBirth = document.getElementById("#birth-empty");
 const emptyPartipation = document.getElementById("#participation-empty");
+const emptyLocations = document.getElementsById("location-empty");
 
 // Regex
 const regexName = /^[a-zA-Z\-àâçéèêëîïôûùüÿñæœ']{2,}$/;
@@ -46,15 +50,25 @@ close.addEventListener("click", function (event) {
   modalbg.style.display = "none";
 });
 
+// Error message
+function errorMessage(value) {
+  value.style.color = "red";
+  value.style.fontSize = "0.7em";
+  value.style.border = "2px solid red";
+}
 
 // Verfication champs de saisie - Prénom
+// firstName.addEventListener("input", function () {
+//   checkFirstName();
+// });
+
 function checkFirstName() {
-  if (emptyFirstName == "") {
-    this.textContent = "Veuillez saisir votre Prénom";
-    this.style.display = "block";
+  if (firstName == "") {
+    errorMessage(emptyFirstName).innerHTML = "Veuillez saisir votre Prénom";
     return false;
   } else if (regexName == false) {
-    emptyFirstName.textContent = "Veuillez saisir un minimum de 2 caractère";
+    errorMessage(emptyFirstName).innerHTML =
+      "Veuillez saisir un minimum de 2 caractère";
     emptyFirstName.style.display = "block";
   } else {
     emptyFirstName.style.display = "none";
@@ -63,15 +77,19 @@ function checkFirstName() {
 }
 
 // Verfication champs de saisie - Nom
+// lastName.addEventListener("input", function () {
+//   checkLastName();
+// });
+
 function checkLastName() {
-  if (emptyLastName == "") {
-    emptyLastName.innerHTML = "Veuillez saisir votre Nom";
+  if (LastName == "") {
+    errorMessage(emptyLastName).innerHTML = "Veuillez saisir votre Nom";
     emptyLastName.style.display = "block";
     return false;
-    // } else if (){
-    //     emptyLastName.innerHTML = "Veuillez saisir un minimum de 2 caractère";
-    //     emptyLastName.style.display = "block";
-    //     return false;
+  } else if (regexName == false) {
+    errorMessage(emptyLastName).innerHTML =
+      "Veuillez saisir un minimum de 2 caractère";
+    emptyLastName.style.display = "block";
   } else {
     emptyLastName.style.display = "none";
     return true;
@@ -79,14 +97,14 @@ function checkLastName() {
 }
 
 function checkEmail() {
-  if (emptyEmail == "") {
-    emptyEmail.innerHTML = "Veuillez saisir votre Email";
+  if (email == "") {
+    errorMessage(emptyEmail).innerHTML = "Veuillez saisir votre Email";
     emptyEmail.style.display = "block";
     return false;
-    // } else if (){
-    //     emptyEmail.innerHTML = "Veuillez saisir une adresse email valide";
-    //     emptyEmail.style.display = "block";
-    //     return false;
+  } else if (regexEmail == false) {
+    errorMessage(emptyEmail).innerHTML =
+      "Veuillez saisir une adresse email valide";
+    emptyEmail.style.display = "block";
   } else {
     emptyEmail.style.display = "none";
     return true;
@@ -94,8 +112,9 @@ function checkEmail() {
 }
 
 function checkBirth() {
-  if (emptyBirth == "") {
-    emptyBirth.innerHTML = "Veuillez saisir votre Date de Naissance";
+  if (birth == "") {
+    errorMessage(emptyBirth).innerHTML =
+      "Veuillez saisir votre Date de Naissance";
     emptyBirth.style.display = "block";
     return false;
   } else {
@@ -105,17 +124,28 @@ function checkBirth() {
 }
 
 function checkParticipation() {
-  if (emptyPartipation == "") {
-    emptyPartipation.innerHTML = "Veuillez saisir votre participation ";
+  if (partipation == "") {
+    errorMessage(emptyPartipation).innerHTML =
+      "Veuillez saisir votre participation ";
     emptyPartipation.style.display = "block";
     return false;
-    // } else if (){
-    //     emptyPartipation.innerHTML = "Veuillez saisir un nombre valide";
-    //     emptyPartipation.style.display = "block";
-    //     return false;
+  } else if (regexParticipation == false) {
+    errorMessage(emptyPartipation).innerHTML =
+      "Veuillez saisir un nombre valide";
+    emptyPartipation.style.display = "block";
   } else {
     emptyPartipation.style.display = "none";
     return true;
+  }
+}
+
+function checkLocations() {
+  if (locations == "") {
+    errorMessage(emptyLocations).innerHTML = "Veuillez choisir une ville";
+    emptyLocations.style.display = "block";
+    return false;
+  } else {
+    emptyLocations.style.display = "none";
   }
 }
 
@@ -126,13 +156,15 @@ function validate() {
   let resultEmail = checkEmail();
   let resulBirth = checkBirth();
   let resultParticipation = checkParticipation();
+  let resultLocations = checkLocations();
 
   if (
     resultFirstName &&
     resultLastName &&
     resultEmail &&
     resulBirth &&
-    resultParticipation
+    resultParticipation &&
+    resultLocations
   ) {
     modalbg.style.display = "none";
     return true;
